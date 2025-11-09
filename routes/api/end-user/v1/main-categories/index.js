bde1cb75df63f62f5f6d6e3966d459de4bac9da8
sachchin06 <sachchin06@gmail.com>
2025-11-09 23:35:23 +0000

    end user api added for main, sub categories

---
+++ routes/api/end-user/v1/main-categories/index.js
@@ -0,0 +1,81 @@
+"use strict";
+const moment = require("moment");
+
+module.exports = async function (fastify, opts) {
+  fastify.get(
+    "",
+    {
+      schema: {
+        tags: ["End User"],
+        security: [{ bearerAuth: [] }],
+        query: {
+          type: "object",
+          properties: {
+            page: {
+              type: "integer",
+              default: 1,
+            },
+            limit: {
+              type: "integer",
+              default: 10,
+            },
+            search: {
+              type: "string",
+              default: "",
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        // await fastify.token.isAuth(request);
+
+        const page = request.query.page;
+        const limit = request.query.limit;
+        const search = request.query.search;
+        const skip = (page - 1) * limit;
+
+        var where = {
+          is_enabled: true,
+          deleted_at: null,
+          OR: [{ name: { contains: search } }],
+        };
+
+        const categories = await fastify.prisma.categories.findMany({
+          where: where,
+          skip: skip,
+          take: limit,
+        });
+
+        const totalCount = await fastify.prisma.categories.count({
+          where: where,
+        });
+
+        const allCount = await fastify.prisma.categories.count({
+          where: {
+            deleted_at: null,
+          },
+        });
+
+        var count = {};
+        count.all = allCount;
+        const totalPages = Math.ceil(totalCount / limit);
+
+        var res = {};
+        res.page = page;
+        res.limit = limit;
+        res.totalPages = totalPages;
+        res.totalCount = totalCount;
+        res.count = count;
+        res.data = categories;
+
+        reply.send(res);
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+};
