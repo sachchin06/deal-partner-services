5071220482d86cdd91411a582f257bafbb898bda
sachchin06 <sachchin06@gmail.com>
2025-11-20 21:55:36 +0000

    code refactored

--- plugins/cors.js
+++ plugins/cors.js
@@ -4,10 +4,6 @@ const fp = require("fastify-plugin");
 
 module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/cors"), {
-    origin: [
-      "https://dev.api.dealpartner.lk",
-      "http://127.0.0.1:3000",
-      "http://localhost:8500",
-    ],
+    origin: ["http://127.0.0.1:3000", "http://localhost:8500"],
   });
 });
